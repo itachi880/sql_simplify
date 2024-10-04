d2c3ea5d010f04d1d843247d52a44d5fb4595f72
itachi880 <bdortiana880@gmail.com>
2024-10-04 12:03:16 +0100

    almost done types and intellessens work fine

--- class.js
+++ class.js
@@ -5,34 +5,55 @@ const { wraper } = require("./mysql_promise");
  * @property {number} warningCount - The number of warnings that occurred during the execution.
  * @property {string} message - A message summarizing the execution (optional).
  * @property {number} serverStatus - The status of the server after the query execution.
+ */
+
+/**
  * @typedef {object} Update
  * @property {number} affectedRows - The number of rows that matched the update condition.
  * @property {number} changedRows - The number of rows that were actually changed (i.e., new values differ from existing).
  * @property {number} warningCount - The number of warnings that occurred during the query execution.
  * @property {string} message - A summary of the execution, typically containing row match/change counts.
  * @property {number} serverStatus - The status of the server after the query execution.
+ */
 
+/**
  * @typedef {object} Insert
  * @property {number} affectedRows - The number of rows that were inserted.
  * @property {number} insertId - The ID of the inserted row, particularly useful for tables with an AUTO_INCREMENT field.
  * @property {number} warningCount - The number of warnings that occurred during the query execution.
  * @property {string} message - A summary of the execution, such as the number of inserted rows.
  * @property {number} serverStatus - The status of the server after the query execution.
- * @template Table_columns
+ */
+/**
+ * @template  default_val_column
+ */
+/**
+ * @typedef {Object} ColumnDefinition
+ * @property {string} type - The data type of the column (e.g., 'integer', 'string').
+ * @property {boolean} autoInc - Indicates if the column is auto-incrementing.
+ * @property {default_val_column} default - The default value for the column.
+ * @property {boolean} primary_key - Indicates if this column is a primary key.
+ */
+/**
+ * @template  Table_columns
  */
 module.exports.Table = class Table {
+  static types = {
+    string: "VARCHAR(255)",
+    integer: "INT",
+    float: "FLOAT",
+    boolean: "BOOL",
+    object: "LONGTEXT",
+  };
   /**
    * @param {object} param0
-   * @param {Table_columns} param0.schema
+   * @param {Record<keyof Table_columns, ColumnDefinition>} param0.schema - The table schema, where each key is a column name and the value is a ColumnDefinition.
    * @param {import("mysql").Pool} param0.db_connection
    * @param {string} param0.table_name
-   * @param {string} param0.table_id_column_name
    */
-  constructor({ schema = {}, db_connection = false, table_name = "", table_id_column_name = "" }) {
+  constructor({ schema = {}, db_connection = false, table_name = "" }) {
     this.schema = schema;
     this.table_name = table_name;
-
-    if (table_id_column_name) this.table_id_name = table_id_column_name;
     /**
      * @type {(sql:string)=>Promise<[response<any>,import("mysql").MysqlError|null]>}
      */
@@ -40,8 +61,12 @@ module.exports.Table = class Table {
     //check for table if exist if not create it
   }
   /**
-   * @param {Table_columns} obj
-   * @returns {Promise<[Table_columns,(import("mysql").MysqlError|null)]>}
+   * @typedef {Record.<keyof Table_columns, ColumnDefinition["default"]>} TableDataType
+   */
+  /**
+   * Creates a new instance of the table.
+   * @param {TableDataType} obj - An object containing the values to insert.
+   * @returns {Promise<[(Insert|null), (import("mysql").MysqlError|null)]>}
    */
   async create(obj) {
     /**
@@ -60,7 +85,7 @@ module.exports.Table = class Table {
     return [obj, null];
   }
   /**
-   * @returns {Promise<[Table_columns[]|null,(import("mysql").MysqlError|null)]>}
+   * @returns {Promise<[TableDataType[]|null,(import("mysql").MysqlError|null)]>}
    */
   async findAll() {
     return await this.db_connection(`SELECT * FROM ${this.table_name}`);
@@ -70,29 +95,34 @@ module.exports.Table = class Table {
    * @typedef {object} Schema
    * @property {string} value - Description of the column property.
    * @property {string} operateur - Description of the column property.
-   * @typedef {Record<keyof T, Schema>} All
+   * @typedef {Record<keyof TableDataType, Schema>} All
    * @typedef {object} Condition
    * @property {(Condition|fullcondition)[]} and - Logical `AND` conditions
    * @property {(Condition|fullcondition)[]} or - Logical `OR` conditions
    * @typedef {(All & Condition)} fullcondition
    * @param {Condition} obj
    * @returns {Promise<[Table_columns[]|null,(import("mysql").MysqlError|null)]>}
+   *
    **/
   async findBy(obj) {
     return await this.db_connection(`SELECT * FROM ${this.table_name} WHERE ${Table.#parse_condition(obj)}`);
   }
   /**
-   * @param {Table_columns} old
-   * @param {Condition} by
-   * @returns {Table_columns}
+   * Updates existing rows in the database.
+   * @param {TableDataType} old - An object containing the new values to set.
+   * @param {fullcondition} by - The condition to identify which rows to update.
+   * @returns {Promise<[Update|null, (import("mysql").MysqlError|null)]>}
+   * @example
+   * update({column_name:"new val"},{id:1})
    */
+
   async update(old = {}, by = {}) {
     return await this.db_connection(`
       UPDATE  ${this.table_name}
       SET ${Object.entries(old)
-        .map((columnUpd) => columnUpd[0] + "=" + '"' + Table.#escapeChar(value) + '"')
+        .map((columnUpd) => columnUpd[0] + "=" + '"' + Table.#escapeChar(columnUpd[1]) + '"')
         .join(",")}
-      ${Object.keys(by).length != 0 ? "WHERE " + Table.#parse_condition(by) : ""}`);
+      ${Object.keys(by).length != 0 ? " WHERE " + Table.#parse_condition(by) : ""}`);
   }
   /**
    * @param {fullcondition} if_is can be {} if the deletion has to affect every column be carfull what you put here
@@ -125,31 +155,37 @@ module.exports.Table = class Table {
     if (error) console.log("error :" + error.message, " \nquery : " + error.sqlMessage);
     return [data, error];
   }
+  async row_sql(sql = "") {
+    return await this.db_connection(Table.#escapeChar(sql));
+  }
   //helper functions
   /**
-   * @param {Table_columns} schema
+   * @param {Record<keyof Table_columns, ColumnDefinition>} schema
    * @returns
    */
-  static #getcolumns_data_type_sting(schema) {
-    let columns = [];
-    const types = {
-      string: "VARCHAR(255)",
-      number: "INT",
-      boolean: "BOOL",
-      object: "LONGTEXT",
-    };
-    for (const [key, type] of Object.entries(schema)) {
-      columns.push(key + " " + types[typeof type]);
+  static #getcolumns_data_type_sting(schema = { autoInc: false, primary_key: false }) {
+    const columns = [];
+    const primary_key = [];
+    for (const [key, defenetion] of Object.entries(schema)) {
+      columns.push(key + " " + defenetion.type + (defenetion.default !== undefined ? " DEFAULT " + '"' + defenetion.default + '"' : "") + (defenetion.autoInc ? " AUTO_INCREMENT " : ""));
+      if (defenetion.primary_key) primary_key.push(key);
     }
+    columns.push("CONSTRAINT PK_cons PRIMARY KEY (" + primary_key.join(" , ") + ")");
     return columns.join(" , ");
   }
+  /**
+   *
+   * @param {Condition} condition
+   * @returns
+   */
   static #parse_condition(condition) {
     const sql = [];
     for (const [key, val] of Object.entries(condition)) {
       if (key === "and" || key === "or") {
         sql.push(`(${val.map(Table.#parse_condition).join(" " + key + " ")})`);
       } else {
-        sql.push(`${key}${val.operateur}"${Table.#escapeChar(val.value + "")}"`);
+        console.log(key, val);
+        sql.push(`${key} ${val.operateur}"${Table.#escapeChar(val.value + "")}"`);
       }
     }
     return sql.join(" and ");
