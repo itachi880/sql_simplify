22d7174eb45867e757e9b71f755e8d029035a01d
itachi880 <badortiana880@gmail.cm>
2024-10-05 18:07:43 +0100

    start documentation

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ const { Table } = require("./class");
 const { wraper } = require("./mysql_promise");
 module.exports.Table = Table;
 module.exports.mysql_promise = wraper;
+function parse_condition(condition) {
+  const sql = [];
+  for (const [key, val] of Object.entries(condition)) {
+    if (key === "and" || key === "or") {
+      sql.push(`(${val.map(parse_condition).join(" " + key + " ")})`);
+    } else {
+      sql.push(`${key} ${val.operateur} " ${val.value + ""}"`);
+    }
+  }
+  return sql.join(" and ");
+}
+console.log(
+  parse_condition({
+    and: [
+      { fieald1: { value: 10, operateur: "=" } },
+
+      {
+        or: [
+          {
+            feald2: { value: "itachi", operateur: "=" },
+          },
+          { feald3: { value: "2022-01-10", operateur: "<" } },
+        ],
+      },
+    ],
+  })
+);
