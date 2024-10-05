55173d7ae657d31fc38df3c085ad420c5d2018de
itachi880 <142436626+itachi880@users.noreply.github.com>
2024-10-05 16:52:12 +0100

    empty commit 1

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const mysql = require("mysql");
 const schema = { name: "", id: 0 };
 const Pool = mysql.createPool({ host: "localhost", user: "root", password: "", database: "test", port: 3306 });
 const Test_module = new Table({ table_id_column_name: "id", schema: { id: { autoInc: true, primary_key: true, type: Table.types.integer }, name: { default: "uknown", type: Table.types.string } }, table_name: "test", db_connection: Pool });
-Test_module.update({ name: "iliass" }, { and: [{}] });
+Test_module.delete({ id: { operateur: "=", value: 1 } }).then((res) => {
+  console.log(res);
+});
